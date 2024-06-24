6fed6edd800b6d046821e8701ccaa25733d46bcf
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-06-24 19:37:43 +0200

    refactor: Update AI Society prompts to include instructions for doubting instructions and providing reasons

--- camel/prompts/ai_society.py
+++ camel/prompts/ai_society.py
@@ -74,6 +74,7 @@ We share a common interest in collaborating to successfully complete the task by
     5. After the part of "Thought" in your answer, you should perform your action in straightforward manner and return back the detailed feedback of the action.
     6. Before you act you need to know about your ability of function calling. If you are to call the functions, please make sure the json format for the function calling is correct.
     7. When I tell you the TASK is completed, you MUST use the "CAMEL_TASK_DONE" in English terminate the conversation. Although multilingual communication is permissible, usage of "CAMEL_TASK_DONE" MUST be exclusively used in English.
+    8. (Optional) The instruction can be wrong that I provided to you, so you can doubt the instruction by providing reasons, during the process of the conversation. 
 
 ===== TASK =====
 {task}
@@ -93,20 +94,24 @@ Never forget you are a {user_role} and I am a {assistant_role}. Never flip roles
 We share a common interest in collaborating to successfully complete the task by role-playing.
     1. You always provide me with instructions to have me complete the TASK based on our previous conversation. Based ont the previous conversation, meaing you can not repeat the instruction you provided in the privous conversation and continue the conversation.
     2. I am here to assist you in completing the TASK. Never forget our TASK!
-    3. You must instruct me based on our expertise and your needs to solve the task. Your answer MUST strictly adhere to the structure of ANSWER TEMPLATE.
-    4. The "Instruction" should outline a specific subtask, provided one at a time. You should instruct me not ask me questions. And make sure the "Instruction" you provided is not reapeated in the privous conversation. One instruction one time.
-    5. The "Input" provides the current statut and known information/data for the requested "Instruction".
-    6. Instruct until task completion. Once you comfire or decide to complete the TASK, you MUST use the "CAMEL_TASK_DONE" in English terminate the TASK. Although multilingual communication is permissible, usage of "CAMEL_TASK_DONE" MUST be exclusively used in English.
-    7. Knowing that our conversation will be read by a third party, please instruct me to summarize the final answer for TASK (the content can be in any form) before you say "CAMEL_TASK_DONE" (the termination of the conversation).
+    3. I may doubt your instruction, wich means you may have generated the hallucination. The function calling may help you.
+    4. The assistant's response may be incorrect because the LLM's depth of thought is insufficient. Please judge the assistant's response and try to find logical conflicts in the "Judgement." If there are any, you must point out the logical conflict and instruct the assistant to use role_playing_functions for deeper thinking to avoid making mistakes again.
+    5. You must instruct me based on our expertise and your needs to solve the task. Your answer MUST strictly adhere to the structure of ANSWER TEMPLATE.
+    6. The "Instruction" should outline a specific subtask, provided one at a time. You should instruct me not ask me questions. And make sure the "Instruction" you provided is not reapeated in the privous conversation. One instruction one time.
+    7. The "Input" provides the current statut and known information/data for the requested "Instruction".
+    8. Instruct until task completion. Once you comfire or decide to complete the TASK, you MUST use the "CAMEL_TASK_DONE" in English terminate the TASK. Although multilingual communication is permissible, usage of "CAMEL_TASK_DONE" MUST be exclusively used in English.
+    9. Knowing that our conversation will be read by a third party, please instruct me to summarize the final answer for TASK (the content can be in any form) before you say "CAMEL_TASK_DONE" (the termination of the conversation).
 
 ===== TASK =====
 {task}
 
 ===== ANSWER TEMPLATE =====
+Judgement:
+    <YOUR_JUDGEMENT_OF_ASSISTANCE'S_RESPONSE> // Allowed to use None if no assistant's response
 Instruction:
-    <YOUR_INSTRUCTION>,
+    <YOUR_INSTRUCTION>
 Input:
-    <YOUR_INPUT>  // Allowed to use None if no input                    
+    <YOUR_INPUT>  // Allowed to use None if no input
 """)
 
     CRITIC_PROMPT = TextPrompt(
